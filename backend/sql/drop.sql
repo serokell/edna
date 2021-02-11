022c7903e9770378f61384e66b53024d7ee57e93
Ilya Peresadin <pva701@gmail.com>
2021-02-11 12:32:24 +0300

    Tentative sql schema among with docker-compose file for postgres.

---
+++ backend/sql/drop.sql
@@ -0,0 +1,7 @@
+drop table if exists project cascade;
+drop table if exists test_methodology cascade;
+drop table if exists experiment cascade;
+drop table if exists measurement cascade;
+drop table if exists analysis_method cascade;
+drop table if exists analysis cascade;
+drop table if exists analysis_removed_measurements cascade;
