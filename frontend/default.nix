56f8faa21245c73d9abc18a91661339eccc35405
Christian Höppner <christian.hoppner@serokell.io>
2021-03-04 15:47:14 +0000

    [EDNA-14] Add OCI image packages

--- frontend/default.nix
+++ frontend/default.nix
@@ -1,15 +1,21 @@
 { buildYarnPackage, fetchurl, python2 }:
-let
-  package = buildYarnPackage {
-    SASS_BINARY_PATH = fetchurl {
-      url = "https://github.com/sass/node-sass/releases/download/v4.12.0/linux-x64-64_binding.node";
-      sha256 = "0dl91l414na44h090cgghd06q0j2whlj9h98im2qb9823glq7xff";
-    };
-    buildInputs = [ python2 ];
 
-    src = ./.;
-    yarnBuild = "yarn";
+buildYarnPackage {
+  SASS_BINARY_PATH = fetchurl {
+    url = "https://github.com/sass/node-sass/releases/download/v4.14.1/linux-x64-64_binding.node";
+    sha256 = "0g0dbccs1wdib1nm7cg6x3iqhxfnrk69cnlsqff2d52gcwymmacj";
   };
-in {
-  inherit package;
+
+  src = ./.;
+  buildInputs = [
+    python2
+  ];
+
+  yarnBuildMore = ''
+    yarn build
+  '';
+
+  installPhase = ''
+    mv dist $out
+  '';
 }
