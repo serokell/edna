9afe7affa32d5fd9c25628f613f2862b2a556ea3
Ilya Peresadin <pva701@gmail.com>
2021-03-08 13:12:20 +0300

    Initialise react-router
    Problem: we need to have a client side routing,
    to be able to show pages under /dashboard, /library and /upload
    
    Solution: use react-router as a solution.

---
+++ frontend/src/components/NotFound/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../Header/Header";
+
+export function NotFound() {
+  return (
+    <>
+      <Header />
+      <span style={{ marginTop: "250px" }}>Здесь ничего нет</span>
+    </>
+  );
+}
