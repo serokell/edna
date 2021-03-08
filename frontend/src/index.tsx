5afb56fceb3086ac54f1bc7269eb9bac56aa0f73
Ilya Peresadin <pva701@gmail.com>
2021-03-08 13:12:20 +0300

    Initialise react-router
    Problem: we need to have a client side routing,
    to be able to show pages under /dashboard, /library and /upload
    
    Solution: use react-router as a solution.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,15 @@
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import React from "react";
 import { RecoilRoot } from "recoil";
-import App from "./App";
+import { App } from "./App";
 import "./index.scss";
 
 ReactDOM.render(
-  <RecoilRoot>
-    <App />
-  </RecoilRoot>,
+  <Router>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </Router>,
   document.getElementById("root")
 );
