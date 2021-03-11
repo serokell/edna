aef1930d04d3571e21b56170d95afdad2a37f179
Ilya Peresadin <pva701@gmail.com>
2021-03-11 16:06:59 +0300

    Add preview table
    Problem: we need to conform new mockup with preview table on upload.
    
    Solution: implement raw table, the styling of it will be done in Library related PR.

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,21 +1,16 @@
 import React, { FunctionComponent, ReactElement } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./Header.scss";
-import cx from "classnames";
 import cn from "../../utils/bemUtils";
-import { capitalizeFirstLetter } from "../../utils/utils";
 
 const Header: FunctionComponent = (): ReactElement => {
   const ednaHeader = cn("ednaHeader");
   const loc = useLocation().pathname;
-  const className = (sec: string) =>
-    cx(ednaHeader("menuitem"), {
-      ednaHeader__menuitem_active: loc.startsWith(sec),
-    });
+  const className = (sec: string) => ednaHeader("menuitem", { active: loc.startsWith(sec) });
 
   const renderLink = (path: string) => (
     <Link key={path} to={`/${path}`} className={className(`/${path}`)}>
-      {capitalizeFirstLetter(path)}
+      {path}
     </Link>
   );
   const links = ["dashboard", "upload", "library"];
