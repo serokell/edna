0bde97c54dd95ae065bc377dfab40ea4ec770a75
Ivan Gromakovskii <gie1994@gmail.com>
2021-03-12 18:27:50 +0300

    Split API into smaller types
    Problem: `EdnaEndpoints` already contains a lot of fields and
    we are going to add even more. Soon it will be hard to comprehend
    what's in this type.
    
    Solution: split it into smaller types: one about projects, one about
    methodology. The idea is to have API endpoints per-entity.

--- backend/src/Edna/Web/API.hs
+++ backend/src/Edna/Web/API.hs
@@ -5,6 +5,10 @@ module Edna.Web.API
   ( EdnaAPI
   , EdnaEndpoints (..)
   , ednaAPI
+
+  -- * Subtypes
+  , ProjectEndpoints (..)
+  , MethodologyEndpoints (..)
   ) where
 
 import Universum
@@ -24,72 +28,8 @@ data EdnaEndpoints route = EdnaEndpoints
       :> MultipartForm Mem (MultipartData Mem)
       :> Post '[JSON] [ExperimentalMeasurement]
 
-  , -- | Add a new project.
-    eeAddProject :: route
-      :- "project"
-      :> Summary "Add a new project"
-      :> ReqBody '[JSON] Project
-      :> Post '[JSON] (WithId Project)
-
-  , -- | Update an existing project.
-    eeEditProject :: route
-      :- "project"
-      :> Summary "Update an existing project"
-      :> Capture "projectId" (SqlId Project)
-      :> ReqBody '[JSON] Project
-      :> Put '[JSON] (WithId Project)
-
-  -- TODO: pagination and sorting are just stubs for now (everywhere).
-  -- Most likely we will use @servant-util@ to implement them,
-  -- but let's do it later.
-
-  , -- | Get known projects with optional pagination and sorting
-    eeGetProjects :: route
-      :- "projects"
-      :> Summary "Get known projects"
-      :> QueryParam "page" Word
-      :> QueryParam "size" Word
-      :> QueryParam "sortby" StubSortBy
-      :> Get '[JSON] [WithExtra Project ProjectExtra]
-
-  , -- | Get project data by ID
-    eeGetProject :: route
-      :- "project"
-      :> Summary "Get project data by ID"
-      :> Capture "projectId" (SqlId Project)
-      :> Get '[JSON] (WithExtra Project ProjectExtra)
-
-  , -- | Add a new methodology.
-    eeAddMethodology :: route
-      :- "methodology"
-      :> Summary "Add a new methodology"
-      :> ReqBody '[JSON] TestMethodology
-      :> Post '[JSON] (WithId TestMethodology)
-
-  , -- | Update an existing methodology.
-    eeEditMethodology :: route
-      :- "methodology"
-      :> Summary "Update an existing methodology"
-      :> Capture "methodologyId" (SqlId TestMethodology)
-      :> ReqBody '[JSON] TestMethodology
-      :> Put '[JSON] (WithId TestMethodology)
-
-  , -- | Get known methodologies with optional pagination and sorting
-    eeGetMethodologies :: route
-      :- "methodologies"
-      :> Summary "Get known methodologies"
-      :> QueryParam "page" Word
-      :> QueryParam "size" Word
-      :> QueryParam "sortby" StubSortBy
-      :> Get '[JSON] [WithId TestMethodology]
-
-  , -- | Get methodology data by ID
-    eeGetMethodology :: route
-      :- "methodology"
-      :> Summary "Get methodology data by ID"
-      :> Capture "methodologyId" (SqlId TestMethodology)
-      :> Get '[JSON] (WithId TestMethodology)
-
+  , eeProjectEndpoints :: route :- ProjectAPI
+  , eeMethodologyEndpoints :: route :- MethodologyAPI
   } deriving stock (Generic)
 
 -- | API type specification.
@@ -98,3 +38,79 @@ type EdnaAPI =
 
 ednaAPI :: Proxy EdnaAPI
 ednaAPI = Proxy
+
+-- TODO: pagination and sorting are just stubs for now (everywhere).
+-- Most likely we will use @servant-util@ to implement them,
+-- but let's do it later.
+
+-- | Endpoints related to projects.
+data ProjectEndpoints route = ProjectEndpoints
+  { -- | Add a new project.
+    peAddProject :: route
+      :- "project"
+      :> Summary "Add a new project"
+      :> ReqBody '[JSON] Project
+      :> Post '[JSON] (WithId Project)
+
+  , -- | Update an existing project.
+    peEditProject :: route
+      :- "project"
+      :> Summary "Update an existing project"
+      :> Capture "projectId" (SqlId Project)
+      :> ReqBody '[JSON] Project
+      :> Put '[JSON] (WithId Project)
+
+  , -- | Get known projects with optional pagination and sorting
+    peGetProjects :: route
+      :- "projects"
+      :> Summary "Get known projects"
+      :> QueryParam "page" Word
+      :> QueryParam "size" Word
+      :> QueryParam "sortby" StubSortBy
+      :> Get '[JSON] [WithExtra Project ProjectExtra]
+
+  , -- | Get project data by ID
+    peGetProject :: route
+      :- "project"
+      :> Summary "Get project data by ID"
+      :> Capture "projectId" (SqlId Project)
+      :> Get '[JSON] (WithExtra Project ProjectExtra)
+  } deriving stock (Generic)
+
+type ProjectAPI = ToServant ProjectEndpoints AsApi
+
+-- | Endpoints related to methodologies.
+data MethodologyEndpoints route = MethodologyEndpoints
+  { -- | Add a new methodology.
+    meAddMethodology :: route
+      :- "methodology"
+      :> Summary "Add a new methodology"
+      :> ReqBody '[JSON] TestMethodology
+      :> Post '[JSON] (WithId TestMethodology)
+
+  , -- | Update an existing methodology.
+    meEditMethodology :: route
+      :- "methodology"
+      :> Summary "Update an existing methodology"
+      :> Capture "methodologyId" (SqlId TestMethodology)
+      :> ReqBody '[JSON] TestMethodology
+      :> Put '[JSON] (WithId TestMethodology)
+
+  , -- | Get known methodologies with optional pagination and sorting
+    meGetMethodologies :: route
+      :- "methodologies"
+      :> Summary "Get known methodologies"
+      :> QueryParam "page" Word
+      :> QueryParam "size" Word
+      :> QueryParam "sortby" StubSortBy
+      :> Get '[JSON] [WithId TestMethodology]
+
+  , -- | Get methodology data by ID
+    meGetMethodology :: route
+      :- "methodology"
+      :> Summary "Get methodology data by ID"
+      :> Capture "methodologyId" (SqlId TestMethodology)
+      :> Get '[JSON] (WithId TestMethodology)
+  } deriving stock (Generic)
+
+type MethodologyAPI = ToServant MethodologyEndpoints AsApi
