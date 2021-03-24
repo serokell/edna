92d2bd65615f8be088b329233f5216e21ad4c69a
Ivan Gromakovskii <gie1994@gmail.com>
2021-03-24 19:12:27 +0300

    Add tests for URI parsing
    Problem: we want to be more confident that our parsing of URIs is
    consistent accross the code and matches `renderURI`.
    
    Solution: add simple tests for that.

---
+++ backend/test/Test/Util/URISpec.hs
@@ -0,0 +1,35 @@
+-- | Tests for @Edna.Util.URI@.
+
+module Test.Util.URISpec
+  ( spec
+  ) where
+
+import Universum
+
+import qualified Data.Aeson as Aeson
+import Hedgehog (forAll, test, tripping)
+import Network.URI (URI, nullURI)
+import Test.Hspec (Spec, describe, it, shouldBe, shouldSatisfy)
+import Test.Hspec.Hedgehog (hedgehog)
+
+import Edna.Util.URI (parseURI, renderURI)
+
+import Test.Gen (genURI)
+
+spec :: Spec
+spec = do
+  describe "parseURI" $ do
+    it "accepts empty URI" $
+      parseURI "" `shouldBe` Just nullURI
+    it "accepts relative URI" $ do
+      parseURI "foo.bar" `shouldSatisfy` isJust
+      parseURI "foo" `shouldSatisfy` isJust
+    it "is reverse for 'renderURI'" $ hedgehog $ do
+      uri <- forAll genURI
+      test $ tripping uri renderURI parseURI
+  describe "JSON parsing" $ do
+    it "accepts empty URI" $
+      Aeson.decode "\"\"" `shouldBe` Just nullURI
+    it "accepts relative URI" $ do
+      Aeson.decode @URI "\"foo.bar\"" `shouldSatisfy` isJust
+      Aeson.decode @URI "\"foo\"" `shouldSatisfy` isJust
