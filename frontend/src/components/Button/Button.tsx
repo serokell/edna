8d08c190cf6914e5dc3697579707d567cd5610a3
Ilya Peresadin <pva701@gmail.com>
2021-03-30 21:25:23 +0300

    Implement new layout for Library/Methodologies
    Problem: current layout of Methodologoies doesn't
    match figma mockup.
    
    Solution: implement tables instead of plates.

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -3,10 +3,12 @@ import "../RoundSpinner.scss";
 import "./Button.scss";
 import cx from "classnames";
 
-type ButtonType = "submit" | "primary" | "outlined" | "rounded" | "link";
+type ButtonType = "submit" | "primary" | "outlined" | "rounded" | "small-rounded" | "link";
+type ButtonSize = "default" | "small";
 
 interface ButtonProps {
   type: ButtonType;
+  size?: ButtonSize;
   className?: string;
   disabled?: boolean;
   children: React.ReactNode;
@@ -18,16 +20,19 @@ interface ButtonProps {
 // TODO add sizes
 export function Button({
   type,
+  size,
   className,
   disabled,
   children,
   loading,
   ...props
 }: ButtonProps): React.ReactElement {
+  const btnSize = size ?? "default";
+
   const buttonModifiers = {
     primaryButton: type === "submit" || type === "primary",
     outlinedButton: type === "outlined",
-    roundedButton: type === "rounded",
+    roundedButton: type === "rounded" || type === "small-rounded",
     linkButton: type === "link",
   };
 
@@ -36,7 +41,10 @@ export function Button({
       {...props}
       disabled={disabled}
       type={type === "submit" ? "submit" : "button"}
-      className={cx(buttonModifiers, [className])}
+      className={cx(buttonModifiers, [className], {
+        roundedButton_smallRounding: type === "small-rounded",
+        "roundedButton_size-small": btnSize === "small",
+      })}
     >
       {children} {loading && <div className="round-spinner btn-spinner" />}
     </button>
