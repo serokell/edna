8ac501337b7551120a1370db95e601d6a67baa4c
Ivan Gromakovskii <gie1994@gmail.com>
2021-04-05 22:35:30 +0300

    Update Upload tests
    Problem: UploadSpec has some pending tests that couldn't be
    implemented some time ago due to lack of Library functionality.
    
    Solution: now that we have all Library functionality, we
    update these tests to use Library actions and add implement a
    positive test.

--- backend/test/Test/UploadSpec.hs
+++ backend/test/Test/UploadSpec.hs
@@ -6,43 +6,94 @@ module Test.UploadSpec
 
 import Universum
 
-import qualified Data.ByteString.Lazy as BSL
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
 
-import Test.Hspec (Spec, describe, it, shouldBe, shouldThrow, xit)
+import RIO (runRIO)
+import Test.Hspec (Spec, beforeAllWith, describe, it, shouldBe, shouldThrow)
 
+import qualified Edna.Dashboard.Service as Dashboard
+import qualified Edna.Library.Service as Library
+
+import Edna.Dashboard.Web.Types (ExperimentResp(..), ExperimentsResp(..))
 import Edna.ExperimentReader.Types (FileContents(..))
+import Edna.Library.Web.Types (CompoundResp(crName), TargetResp(..))
 import Edna.Upload.Service (UploadError(..), parseFile', uploadFile')
 import Edna.Upload.Web.Types (FileSummary(..), FileSummaryItem(..), NameAndId(..), sortFileSummary)
-import Edna.Util (SqlId(..))
+import Edna.Util (IdType(..), SqlId(..))
+import Edna.Web.Types (WithId(..))
 
 import Test.SampleData
-import Test.Setup (runWithInit, withContext)
+import Test.Setup (runTestEdna, runWithInit, withContext)
 
 spec :: Spec
-spec = withContext $ do
+spec = withContext $ startWithInitial $ do
   describe "parseFile'" $ do
-    it "returns empty summary for empty contents" $ \ctx -> do
-      summary <- runWithInit ctx $ parseFile' (FileContents mempty sampleMetadata)
-      summary `shouldBe` FileSummary []
-    it "returns correct summary for sample data" $ \ctx -> do
-      summary <- runWithInit ctx $ parseFile' sampleFile
-      sortFileSummary summary `shouldBe` sortFileSummary sampleFileSummary
-    -- TODO Need to be able to add projects and methodologies.
-    xit "returns IDs when data is known" $ \ctx -> do
-      summary <- runWithInit ctx $ uploadSampleFile >> parseFile' sampleFile
-      sortFileSummary summary `shouldBe` sortFileSummary sampleFileSummary2
-  describe "uploadFile'" $ do
+    it "returns empty summary for empty contents" $ runTestEdna $ do
+      summary <- parseFile' (FileContents mempty sampleMetadata)
+      liftIO $ summary `shouldBe` FileSummary []
+    it "returns correct summary for sample data" $ runTestEdna $ do
+      summary <- parseFile' sampleFile
+      liftIO $
+        summary `shouldBe` sortFileSummary sampleFileSummary
+    it "returns IDs when data is known" $ runTestEdna $ do
+      summary <-
+        uploadFileTest (SqlId 1) (SqlId 1) sampleFile >> parseFile' sampleFile
+      liftIO $
+        summary `shouldBe` sortFileSummary sampleFileSummary'
+  -- Starting with initial state here because there is one successful upload above.
+  describe "uploadFile'" $ startWithInitial $ do
     it "fails when referenced project does not exist" $ \ctx -> do
-      runWithInit ctx uploadSampleFile `shouldThrow`
-        (== UEUnknownProject (SqlId 1))
-    xit "fails when referenced test methodology does not exist" $ \ctx -> do
-      runWithInit ctx uploadSampleFile `shouldThrow`
-        (== UEUnknownTestMethodology (SqlId 1))
-    xit "TODO: positive tests" $ \ctx ->
-      runWithInit ctx pass
+      runRIO ctx (uploadFileTest unknownSqlId (SqlId 1) sampleFile) `shouldThrow`
+        (== UEUnknownProject unknownSqlId)
+    it "fails when referenced test methodology does not exist" $ \ctx -> do
+      runRIO ctx (uploadFileTest (SqlId 1) unknownSqlId sampleFile) `shouldThrow`
+        (== UEUnknownTestMethodology unknownSqlId)
+    -- Note that there are some checks in LibrarySpec and DashboardSpec as well.
+    it "successfully adds targets and compounds" $ runTestEdna $ do
+      summary <- uploadFile' (SqlId 1) (SqlId 1) "file description" "file name"
+        "blob" sampleFile
+      summary2 <- uploadFile' (SqlId 2) (SqlId 2) "file description 2" "file name 2"
+        "blob2" sampleFile2
+      targets <- Library.getTargets Nothing Nothing Nothing
+      compounds <- Library.getCompounds Nothing Nothing Nothing
+      experiments <- Dashboard.getExperiments Nothing Nothing Nothing
+      liftIO $ do
+        summary `shouldBe` sortFileSummary sampleFileSummary'
+        summary2 `shouldBe` sortFileSummary sampleFileSummary2
+        checkTargets targets
+        checkCompounds compounds
+        checkExperiments (wItem <$> erExperiments experiments)
   where
-    uploadSampleFile =
-      uploadFile' (SqlId 1) (SqlId 1) "descr" "name" BSL.empty sampleFile
+    addInitialData = addSampleProjects >> addSampleMethodologies
+    startWithInitial =
+      beforeAllWith (\ctx -> ctx <$ runWithInit ctx addInitialData)
+
+    checkTargets :: [WithId 'TargetId TargetResp] -> IO ()
+    checkTargets responses =
+      Map.fromList (map (\WithId {..} -> (trName wItem, trProjects wItem)) responses) `shouldBe`
+      expectedTargets
+
+    expectedTargets = Map.fromList
+      [ (targetName1, [projectName1, projectName2 ])
+      , (targetName2, [projectName1])
+      , (targetName3, [projectName1])
+      , (targetName4, [projectName2])
+      ]
+
+    checkCompounds :: [WithId 'CompoundId CompoundResp] -> IO ()
+    checkCompounds responses =
+      Set.fromList (map (\WithId {..} -> crName wItem) responses) `shouldBe`
+      Set.fromList [compoundName1, compoundName2, compoundName3, compoundName4, compoundName5]
+
+    checkExperiments :: [ExperimentResp] -> IO ()
+    checkExperiments responses = do
+      length (filter (\ExperimentResp {..} -> erProject == SqlId 1) responses)
+        `shouldBe` 6
+      length (filter (\ExperimentResp {..} -> erProject == SqlId 2) responses)
+        `shouldBe` 3
+      forM_ responses $ \ExperimentResp {..} ->
+        length erSubExperiments `shouldBe` 1
 
 newNAI :: Text -> NameAndId anything
 newNAI name = NameAndId name Nothing
@@ -59,11 +110,18 @@ sampleFileSummary = FileSummary
 oldNAI :: Word32 -> Text -> NameAndId anything
 oldNAI i name = NameAndId name (Just (SqlId i))
 
-sampleFileSummary2 :: FileSummary
-sampleFileSummary2 = FileSummary
+sampleFileSummary' :: FileSummary
+sampleFileSummary' = FileSummary
   [ FileSummaryItem (oldNAI 1 targetName1)
       [oldNAI 1 compoundName1, oldNAI 2 compoundName2, oldNAI 3 compoundName3]
   , FileSummaryItem (oldNAI 2 targetName2) [oldNAI 2 compoundName2]
   , FileSummaryItem (oldNAI 3 targetName3)
       [oldNAI 1 compoundName1, oldNAI 4 compoundName4]
+  ]
+
+sampleFileSummary2 :: FileSummary
+sampleFileSummary2 = FileSummary
+  [ FileSummaryItem (oldNAI 1 targetName1) [oldNAI 2 compoundName2]
+  , FileSummaryItem (oldNAI 5 targetName4)
+      [oldNAI 1 compoundName1, oldNAI 7 compoundName5]
   ]
