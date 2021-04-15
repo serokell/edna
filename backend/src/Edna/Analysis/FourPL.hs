e3a523dc56a6b14061ce386f5b3ae9edc5b36203
Ivan Gromakovskii <gie1994@gmail.com>
2021-04-15 12:59:50 +0300

    Update edna-server to handle new Python analysis
    Problem: Python analysis code was updated, now it find suspicious
    points and has a different output format. Our Haskell code doesn't
    handle this new format.
    
    Solution:
    1. Update FourPL.hs to parse new data format and output more data.
    2. Add `analyse4PLOne` function that works on one sub-experiment
    to simplify usage of analysis in this case.
    3. Extract details of Python calling into a separate module in
    preparation for even more Python code.
    4. Update Upload service to automatically create an additional
    sub-experiment when at least one suspicious point is found.

--- backend/src/Edna/Analysis/FourPL.hs
+++ backend/src/Edna/Analysis/FourPL.hs
@@ -6,24 +6,24 @@ module Edna.Analysis.FourPL
   , Params4PLResp (..)
   , Params4PLReq (..)
   , analyse4PL
+  , analyse4PLOne
   ) where
 
 import Universum
 
-import Data.Aeson (FromJSON(..), ToJSON(..), eitherDecode, encode)
-import Data.Aeson.TH (deriveJSON)
+import Data.Aeson (FromJSON(..), ToJSON(..))
+import Data.Aeson.TH (deriveFromJSON, deriveToJSON)
 import Data.Swagger (ToSchema(..))
 import Fmt (Buildable(..), genericF, tupleF)
 import Servant.Util.Combinators.Logging (ForResponseLog, buildForResponse)
-import System.Environment (lookupEnv)
-import System.Exit (ExitCode(..))
-import System.FilePath ((</>))
-import System.Process (StdStream(..), cwd, proc, readCreateProcessWithExitCode, std_err, std_out)
 
-import Edna.Logging (logDebug)
-import Edna.Orphans ()
+import Edna.Analysis.Infra (PythonError(..), callPythonAnalysis)
 import Edna.Setup (Edna)
-import Edna.Util (ExperimentId, ednaAesonWebOptions)
+import Edna.Util (ExperimentId, SqlId(..), ednaAesonPythonOptions)
+
+--------------------------------
+-- Params4PL and AnalysisResult
+--------------------------------
 
 -- | Parameters of 4PL function, analysis outcome of this analysis method.
 -- The function is defined as follows:
@@ -70,39 +70,71 @@ instance Buildable AnalysisResult where
 instance Buildable (ForResponseLog AnalysisResult) where
   build = buildForResponse
 
+--------------------------------
+-- Analysis interface and implementation
+--------------------------------
+
+-- | Data submitted for analysis of one experiment.
 data Params4PLReq = Params4PLReq
   { plreqExperiment :: ExperimentId
+  , plreqFindOutliers :: Bool
   , plreqData :: [(Double, Double)]
   } deriving stock (Generic, Show, Eq)
 
-deriveJSON ednaAesonWebOptions ''Params4PLReq
-
-data Params4PLResp = Params4PLResp
-  { plrspExperiment :: ExperimentId
-  , plrspData :: AnalysisResult
+-- | Data returned by Python code for each experiment.
+data Params4PLRespPy = Params4PLRespPy
+  { plrspyExperiment :: ExperimentId
+  , plrspyStatus :: Text
+  , plrspyParams :: Maybe Params4PL
+  , plrspyOutliers :: Maybe [Word]
+  , plrspyNewParams :: Maybe Params4PL
   } deriving stock (Generic, Show, Eq)
 
-deriveJSON ednaAesonWebOptions ''Params4PLResp
+deriveToJSON ednaAesonPythonOptions ''Params4PLReq
+deriveFromJSON ednaAesonPythonOptions ''Params4PLRespPy
 
-callPythonAnalysis :: String -> Edna (Either String [Params4PLResp])
-callPythonAnalysis request = do
-  logDebug $ toText $ "Python data request: " <> request
-  analysisDir <- liftIO $ fromMaybe (".." </> "analysis") <$> (lookupEnv "EDNA_ANALYSIS_DIR")
-  (exitCode, out, err) <- liftIO $ readCreateProcessWithExitCode
-    (proc "python3" ["analysis" </> "ic50.py", request])
-    { cwd = Just analysisDir, std_out = CreatePipe, std_err = CreatePipe}
-    ""
-  unless (null err) $ logDebug $ "Unexpected python errors: " <> toText err
-  pure $ do
-    unless (exitCode == ExitSuccess) $
-      Left $ "python call exited with " <>
-      show exitCode <> ", stderr: " <> err
-    eitherDecode $ encodeUtf8 $ toString out
+-- | Data returned from 'analyse4PL' for each experiment.
+data Params4PLResp = Params4PLResp
+  { plrspParams :: Params4PL
+  -- ^ Parameters computed for given experiment.
+  , plrspNewSubExp :: Maybe (NonEmpty Word, Params4PL)
+  -- ^ Information about optional new sub-experiment: indices of outliers and
+  -- new 4PL parameters.
+  } deriving stock (Generic, Show, Eq)
 
--- | This function performs actual analysis and finds the best parameters.
-analyse4PL :: [Params4PLReq] -> Edna [Params4PLResp]
-analyse4PL experiments = do
-  analysisResults <- callPythonAnalysis $ decodeUtf8 $ encode experiments
-  case analysisResults of
-    Left parsingError -> error $ toText parsingError
-    Right params -> pure params
+-- | This function analyses data points and for each experiment computes
+-- the following:
+--
+-- 1. Best 4PL parameters for given points.
+-- 2. If 'plreqFindOutliers' is enabled, it tries to find points that look like
+-- outliers.
+-- 3. If any outliers are found, it computes 4PL parameters for points without
+-- outliers.
+--
+-- Note: there are two different notions of outliers:
+--
+-- 1. Some points are explicitly marked as outliers in experiment data files.
+-- They are disabled by default.
+-- 2. In addition to that, our analysis may automatically detect suspicious points
+-- that we treat as "likely outliers".
+analyse4PL :: [Params4PLReq] -> Edna [(ExperimentId, Either Text Params4PLResp)]
+analyse4PL requests =
+  callPythonAnalysis "ic50_analysis.py" requests >>= mapM respPyToResp
+  where
+    respPyToResp :: Params4PLRespPy -> Edna (ExperimentId, Either Text Params4PLResp)
+    respPyToResp Params4PLRespPy{..} = (plrspyExperiment,) <$> case plrspyStatus of
+      "DONE" -> do
+        plrspParams <- whenNothing plrspyParams $
+          throwM $ PyInvalidFormat "no params in successful computation"
+        let plrspNewSubExp = (,) <$> (nonEmpty =<< plrspyOutliers) <*> plrspyNewParams
+        return $ Right Params4PLResp {..}
+      err -> return $ Left err
+
+-- | A simplified version of 'analyse4PL' for analysis of one sub-experiment.
+-- It doesn't care about IDs and outliers.
+analyse4PLOne :: [(Double, Double)] -> Edna AnalysisResult
+analyse4PLOne points = do
+  analyse4PL [Params4PLReq (SqlId 1) False points] >>= \case
+    [(_, eitherResp)] -> pure $ plrspParams <$> eitherResp
+    resps -> throwM . PyInvalidFormat $
+      "Unexpected number of responses for single request " <> show (length resps)
---
+++ backend/src/Edna/Analysis/Infra.hs
@@ -0,0 +1,62 @@
+-- | Infrastructure to call analysis code in Python.
+
+module Edna.Analysis.Infra
+  ( PythonError (..)
+  , callPythonAnalysis
+  ) where
+
+import Universum
+
+import Data.Aeson (FromJSON(..), ToJSON(..), eitherDecode, encode)
+import Fmt (Buildable(..), pretty)
+import System.Environment (lookupEnv)
+import System.Exit (ExitCode(..))
+import System.FilePath ((</>))
+import System.Process (StdStream(..), cwd, proc, readCreateProcessWithExitCode, std_err, std_out)
+
+import Edna.Logging (logDebug)
+import Edna.Setup (Edna)
+
+-- | General errors that can happen when we call Python code.
+-- They indicate a bug in Python code or incorrect environment (e. g. mismatched
+-- versions of Haskell and Python code).
+data PythonError
+  = PyExitFailure ExitCode
+  -- ^ Python process exited with non-zero code.
+  | PyInvalidFormat Text
+  -- ^ Python process produced output that we can't parse.
+  deriving stock (Show, Eq)
+
+instance Buildable PythonError where
+  build = \case
+    PyExitFailure ec -> "python call exited with " <> build (show @String ec)
+    PyInvalidFormat err -> "failed to decode python output: " <> build err
+
+instance Exception PythonError where
+  displayException = pretty
+
+-- | Call any Python script assuming the following interface:
+--
+-- 1. Input to the script is JSON-encoded and passed as the first command line
+-- argument.
+-- 2. Output is read from stdout and parsed from JSON.
+--
+-- If output decoding fails or Python exits with non-zero code, an error is
+-- returned as a @Left@ value.
+-- Path passed to this function must be relative to the entry point for Python
+-- code (where Poetry config is located).
+callPythonAnalysis ::
+  (ToJSON inp, FromJSON out) => FilePath -> inp -> Edna out
+callPythonAnalysis pyPath request = do
+  let requestString = decodeUtf8 $ encode request
+  logDebug $ toText $ "Python data request: " <> requestString
+  analysisDir <- liftIO $
+    fromMaybe (".." </> "analysis") <$> lookupEnv "EDNA_ANALYSIS_DIR"
+  (exitCode, out, err) <- liftIO $ readCreateProcessWithExitCode
+    (proc "python3" [pyPath, requestString])
+    { cwd = Just analysisDir, std_out = CreatePipe, std_err = CreatePipe}
+    ""
+  unless (null err) $ logDebug $ "Unexpected python errors: " <> toText err
+  logDebug $ toText $ "Python data response: " <> out
+  unless (exitCode == ExitSuccess) $ throwM $ PyExitFailure exitCode
+  either (throwM . PyInvalidFormat . toText) pure $ eitherDecode (encodeUtf8 out)
