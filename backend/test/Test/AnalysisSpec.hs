2d3285b7aba0259e23dc8af2e662d4476a8791b4
Ivan Gromakovskii <gie1994@gmail.com>
2021-04-19 09:27:31 +0300

    Add simple unit tests for analysis
    Problem: recently added tests (for upload with auto outliers)
    indirectly cover new functionality of `analyse4PL`,
    but they are quite high-level and may fail for many reasons.
    So it's not obvious which tests cover `analyse4PL` functionality
    (especially proper data exchange with Python) and if something
    goes wrong in this functionality it may be hard to figure out the
    source of the bug.
    
    Solution: add a module with unit tests specifically for `analyse4PL`
    and check simple things there.

---
+++ backend/test/Test/AnalysisSpec.hs
@@ -0,0 +1,42 @@
+-- | Unit tests for Analysis modules.
+
+module Test.AnalysisSpec
+  ( spec
+  ) where
+
+import Universum
+
+import Test.Hspec (Spec, describe, it, shouldBe, shouldSatisfy)
+
+import Edna.Analysis.FourPL
+import Edna.ExperimentReader.Types (Measurement(..))
+import Edna.Util (SqlId(..))
+
+import Test.Orphans ()
+import Test.SampleData (autoOutlierMeasurements)
+import Test.Setup (runTestEdna, withContext)
+
+spec :: Spec
+spec = withContext do
+  describe "analyse4PL" $ do
+    it "returns empty result for empty request" $ runTestEdna $
+      analyse4PL [] >>= liftIO . (`shouldBe` [])
+    it "returns an error for empty list of points" $ runTestEdna $ do
+      [(expId, resp)] <- analyse4PL [req []]
+      liftIO $ do
+        expId `shouldBe` experimentId
+        resp `shouldSatisfy` isLeft
+    it "returns a reasonable result for sample data" $ runTestEdna $ do
+      let points = autoOutlierMeasurements <&>
+            \Measurement {..} -> (mConcentration, mSignal)
+      [(expId, Right resp)] <- analyse4PL [req points]
+      liftIO $ do
+        expId `shouldBe` experimentId
+        plrspNewSubExp resp `shouldSatisfy` isJust
+  where
+    experimentId = SqlId 1
+    req points = Params4PLReq
+      { plreqExperiment = experimentId
+      , plreqFindOutliers = True
+      , plreqData = points
+      }
